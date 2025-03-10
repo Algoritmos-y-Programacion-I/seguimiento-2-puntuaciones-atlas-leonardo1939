2f36bd941048c2a47aef4afe7f4035d2ac83e84d
leonardo1939 <leonardoandres1939@gmail.com>
2025-03-09 19:19:48 -0500

    Update AtlasPuntuaciones.java

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -1,79 +1,101 @@
-// Análisis (completar)
-// Descripción del programa:
-// Entradas:
+//Entradas: 
+// Numero de jugadores(Scanner)
+// Numero de Rondas(Scanner)
+// Puntaje de Jugador#
 // Salidas:
-// Ejemplo:
+// Puntaje Final 
+//Este programa sirve para llevar la cuenta de el progreso de los jugadores y proporcionar al final un resultado de cual fueron sus puntajes
+// y dar al ganador 
+//Ejemplo:
+//Ingrese el número de jugadores
+//2
+//Ingrese el número de rondas
+//2
+//Jugador1- Puntaje en ronda 1
+//23
+//Jugador1- Puntaje en ronda 2
+//34
+//Jugador2- Puntaje en ronda 1
+//32
+//Jugador2- Puntaje en ronda 2
+//34
+//Resultados finales
+//Jugador1 Puntaje total: 57
+//Jugador2 Puntaje total: 66
+//El puntaje mas alto fue:
+//Jugador2 con 66 puntos
 
+import java.util.Scanner;
 
-package ui; // No olvidar especificar el paquete / carpeta de nuestro programa
+public class Atlas2 {
+    public static Scanner escaner;
+    public static String[] jugadores;
+    public static int[] puntajes;
 
-import java.util.Scanner; // Para poder usar Scanner, debemos importarlo
-
-public class AtlasPuntuaciones {
-
-	// Declaración del Escaner que nos ayudará en todo el programa
-	private Scanner escaner;
-
-	// Declaración de nuestras constantes a utilizar
-
-
-	private AtlasPuntuaciones() {
-		escaner = new Scanner(System.in);
-	}
-	
-	
-	public void run()
-	{
-		// Declaracion de todas las variables a usar en el programa
-
-
-		// Notificacion al usuario de la solicitud de dato
-
-		// Capturamos el dato con nuestro Scanner (que se llama, escaner)
-
-
-
-		// Realizamos los cálculos delegandolos a los métodos
-
-
-		// Notificación al usuario de los resultados
-
-
-	}
-
-	public static void main(String[] args) {
-		AtlasPuntuaciones mainApp = new AtlasPuntuaciones();
-		mainApp.run();
-	}
-
-	/**
-	 * Descripción: El método calcularSumaTotal permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public int calcularSumaTotal(int[] numeros) {
-        // Completar
+    public static void main(String args[]) {
+        escaner = new Scanner(System.in);
+        int NumeroJugadores = numJugadores();
+        int Rondas = numRondas();
+        jugadores = new String[NumeroJugadores];
+        puntajes = new int[NumeroJugadores];
+        
+        for (int i = 0; i < NumeroJugadores; i++) {
+            jugadores[i] = "Jugador" + (i + 1);
+            puntajes[i] = 0;
+            for (int j = 0; j < Rondas; j++) {
+                puntajes[i] += obtenerPuntaje(jugadores[i], j + 1);
+            }
+        }
+        mostrarResultados();
+        mostrarGanador();
     }
 
-	/**
-	 * Descripción: El método calcularPromedio permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public double calcularPromedio(int[] numeros) {
-        // Completar
+    public static int numJugadores() {
+        int NumeroJugadores;
+        do {
+            System.out.println("Ingrese el Numero de Jugadores");           
+            NumeroJugadores = escaner.nextInt();
+        } while (NumeroJugadores < 1 || NumeroJugadores > 10);
+        return NumeroJugadores;
     }
 
-	/**
-	 * Descripción: El método encontrarMayor permite ...
-	 * @param double[] numeros
-	 * @return 
-	 */
-    public double encontrarMayor(double[] numeros) {
-        // Completar
+    public static int numRondas() {
+        int Rondas;
+        do {
+            System.out.println("Ingrese el Numero de Rondas");
+            Rondas = escaner.nextInt();
+        } while (Rondas < 1 || Rondas > 5);
+        return Rondas;
     }
 
-	
+    public static int obtenerPuntaje(String jugador, int ronda) {
+        int puntaje;
+        do {
+            System.out.println(jugador + "- Puntaje en ronda " + ronda);
+            puntaje = escaner.nextInt();
+        } while (puntaje < 0 || puntaje > 100);
+        return puntaje;
+    }
 
-	
+    public static void mostrarResultados() {
+        System.out.println("Resultados Del Torneo ");
+        for (int i = 0; i < jugadores.length; i++) {
+            System.out.println(jugadores[i] + " Puntaje total: " + puntajes[i]);
+        }
+    }
+
+    public static void mostrarGanador() {
+        if (jugadores.length == 0) return;
+        int MejorPuntaje = puntajes[0];
+        String ganador = jugadores[0];
+
+        for (int i = 1; i < jugadores.length; i++) {
+            if (puntajes[i] > MejorPuntaje ) {
+                MejorPuntaje = puntajes[i];
+                ganador = jugadores[i];
+            }
+        }
+        System.out.println(" El jugador con la puntuación mas alta es: ");
+        System.out.println(ganador + " con " + MejorPuntaje + " puntos");
+    }
 }
